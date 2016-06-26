f1cae922a7f132810440f2ee39c6ea8480824640
Jullia <jullia.pimenta@gmail.com>
2016-06-26 14:07:25 -0300

    adicionando ejs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hoteis', function(req, res) {
+	res.render('hoteis', {title: 'Express'});
+});
+
+router.get('/museus', function(req, res){
+	res.send('museus');
+})
+
+router.get('/parques', function(req, res){
+	res.send('parques');
+})
+
+router.get('/pizzarias', function(req, res){
+	res.send('pizzarias');
+})
+
+router.get('/restaurantes', function(req, res){
+	res.send('restaurantes');
+})
+
+
 module.exports = router;
