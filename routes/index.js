44c3129cb9aff58cb73d4148ba3e11da11afcd97
Jullia <jullia.pimenta@gmail.com>
2016-06-28 22:17:16 -0300

    Revert "alterando para one page layout"
    This reverts commit bf5deee9308bfcbd6599a8ec3189d0fb743fee04.

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { hoteis: hoteis, museus: museus, parques: parques, restaurantes: restaurantes });
+  res.render('index', { title: 'Express' });
 });
+
+router.get('/hoteis', function(req, res) {
+	res.render('hoteis', { hoteis: hoteis });
+});
+
+router.get('/museus', function(req, res){
+	res.render('museus', { museus: museus });
+})
+
+router.get('/parques', function(req, res){
+	res.render('parques', { parques: parques });
+})
+
+
+router.get('/restaurantes', function(req, res){
+	res.render('restaurantes', { restaurantes: restaurantes});
+})
 
 
 module.exports = router;
