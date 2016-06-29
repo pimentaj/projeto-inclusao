93a6bb8896d88994ce719899893e46b62483b7e3
Jullia <jullia.pimenta@gmail.com>
2016-06-29 11:22:03 -0300

    alterando páginas

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var hoteis = require("../hotel.json");
 var museus = require("../museu.json");
-var parques = require("../parques.json")
+var parques = require("../parques.json");
 var restaurantes = require("../restaurantes.json");
 var express = require('express');
 var router = express.Router();
@@ -22,10 +22,8 @@ router.get('/parques', function(req, res){
 	res.render('parques', { parques: parques });
 })
 
-
 router.get('/restaurantes', function(req, res){
 	res.render('restaurantes', { restaurantes: restaurantes});
-})
-
+});
 
 module.exports = router;
