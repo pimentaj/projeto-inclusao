0e9c63152c7943a952a06cbaaf1a745f89ef29f2
Jullia <jullia.pimenta@gmail.com>
2016-06-30 01:06:30 -0300

    deixando menu ativo com base na página atual

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', active: ' ' });
 });
 
 router.get('/hoteis', function(req, res) {
-	res.render('lugares', { lugares: hoteis });
+	res.render('lugares', { lugares: hoteis, active: 'hoteis' });
 });
 
 router.get('/museus', function(req, res){
-	res.render('lugares', { lugares: museus });
+	res.render('lugares', { lugares: museus, active: 'museus' });
 })
 
 router.get('/parques', function(req, res){
-	res.render('lugares', { lugares: parques });
+	res.render('lugares', { lugares: parques, active: 'parques' });
 })
 
 router.get('/restaurantes', function(req, res){
-	res.render('lugares', { lugares: restaurantes});
+	res.render('lugares', { lugares: restaurantes, active: 'restaurantes' });
 });
 
 module.exports = router;
+
